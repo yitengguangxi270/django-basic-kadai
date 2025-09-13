752bee699b6b6019e5ead6af6804ff07d015ab0c
yitengguangxi270 <yitengguangxi270@gmail.com>
2025-09-13 23:05:59 +0900

    Add files via upload

---
+++ kadai_012/crud/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)   
+    img = models.ImageField(blank=True, default='noImage.png')
+     # 👇 この行を追加しました
+    description = models.TextField(blank=True, null=True)
+
+
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('list')
